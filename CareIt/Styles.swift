c2823d0d0033bff688e8365a84f5e15cdd5adcf7
hughes <hugslondergan@gmail.com>
2019-01-03 12:30:39 -0500

    added Styles enum

---
+++ CareIt/Styles.swift
@@ -0,0 +1,13 @@
+//
+//  Styles.swift
+//  CareIt
+//
+//  Created by William Londergan (student LM) on 1/3/19.
+//  Copyright © 2019 Jason Kozarsky (student LM). All rights reserved.
+//
+import Foundation
+
+enum Styles: String {
+    case font = "helvetica neue"
+    case color = "ffffff"
+}
