457d2661bafdd09ffd5a2413eb4a271ec6fa7fbd
Jason Kozarsky (student LM) <s012795@lm-19s012795.lmsd.org>
2019-01-03 11:56:19 -0500

    Initial Commit

---
+++ CareIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CareIt
+//
+//  Created by Jason Kozarsky (student LM) on 1/3/19.
+//  Copyright © 2019 Jason Kozarsky (student LM). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
