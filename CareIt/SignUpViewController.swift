39f75125f6fda46281830d2071eae7b8308c0ac2
Jason Kozarsky <jasonkozarsky19@students.lmsd.org>
2019-01-04 13:40:39 -0500

    Configured Firebase, added log in and sign up view controllers

---
+++ CareIt/SignUpViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SignUpViewController.swift
+//  CareIt
+//
+//  Created by Jason Kozarsky (student LM) on 1/4/19.
+//  Copyright © 2019 Jason Kozarsky (student LM). All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
