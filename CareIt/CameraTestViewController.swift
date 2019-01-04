82281c8f152eb6da1123cb692d73b8655e305edc
Jason Kozarsky <jasonkozarsky19@students.lmsd.org>
2019-01-04 11:19:47 -0500

    Changed comments, no actual code

--- CareIt/CameraTestViewController.swift
+++ CareIt/CameraTestViewController.swift
@@ -13,7 +13,7 @@ class CameraTestViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +21,5 @@ class CameraTestViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
