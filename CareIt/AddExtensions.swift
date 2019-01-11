1ad8aefb457f191da495b5588e86d066bc734400
marshmallowsole <s011490@lm-19s011490.lmsd.org>
2019-01-11 14:08:02 -0500

    Add AddExtensions File for Styles Sheet

---
+++ CareIt/AddExtensions.swift
@@ -0,0 +1,25 @@
+//
+//  AddExtensions.swift
+//  CareIt
+//
+//  Created by Katherine Wang (student LM) on 1/7/19.
+//  Copyright © 2019 Jason Kozarsky (student LM). All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIButton{
+    func setUp() {
+        self.backgroundColor = UIColor.orange
+//        self.titleLabel?.font = UIFont(descriptor: , size: 30)
+        
+    }
+    
+}
+
+extension UILabel{
+    func setUpLabel(){
+        
+    }
+}
