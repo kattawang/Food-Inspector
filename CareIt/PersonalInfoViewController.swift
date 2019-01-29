880595d3c7cdee754fd2a8d294fc0b3826e94274
Annie Liang (student LM) <s015205@lm-20s015205.lmsd.org>
2019-01-29 13:19:01 -0500

    Add Personal Info Page in Storyboard and new file

---
+++ CareIt/PersonalInfoViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PersonalInfoViewController.swift
+//  CareIt
+//
+//  Created by Annie Liang (student LM) on 1/29/19.
+//  Copyright © 2019 Jason Kozarsky (student LM). All rights reserved.
+//
+
+import Foundation
+
+class PersonalInfoViewController{
+    
+}
