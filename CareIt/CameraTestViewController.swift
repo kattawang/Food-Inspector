a9f2647704c358c8c2ef84bdc9d3dbbfb08dd209
Hughes <hugslondergan@gmail.com>
2019-02-01 12:32:23 -0500

    Revert "updated the camera test view controller"
    This reverts commit 1cb4015fab1643caf17a554be8354dfb471645ef.

--- CareIt/CameraTestViewController.swift
+++ CareIt/CameraTestViewController.swift
@@ -23,11 +23,15 @@ class CameraTestViewController:
     var databaseReq: DatabaseRequests?
     
     @IBOutlet var loadingPopover: UIView!
+    
+    @IBOutlet weak var takePictureButton: UIButton!
 
+    @IBOutlet weak var choosePictureButton: UIButton!
     /*
      This is a placeholder right now for what we'll do with the barcode information.
      For now, I'm displaying it after analyzing for debugging purposes.
      */
+    @IBOutlet weak var barcodeText: UILabel!
     
     /*
      There's no outlet to this because it actually can't get added programatically.
@@ -36,13 +40,21 @@ class CameraTestViewController:
      b) camera view
      required to get a picture into the app from elsewhere.
     */
+    var imagePicker: UIImagePickerController = UIImagePickerController()
     var currentImage: UIImage?
+    @IBOutlet weak var analyzePictureButton: UIButton!
     
     /*
      This action allows the user to choose an image from their phone (or emulator)'s local storage
      so that they can analyze it for a barcode. Presumably in the final app people will mostly want
      to analyze things that they take pictures of, but this is useful for the time being.
      */
+    @IBAction func choosePictureTouchedUpInside(_ sender: Any) {
+        analyzePictureButton.isEnabled = true
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .photoLibrary
+        self.present(imagePicker, animated: true, completion: nil)
+    }
     
     /*
      This function may look scary, but really it's just a bunch of gross code. ¯\_(ツ)_/¯
@@ -50,57 +62,81 @@ class CameraTestViewController:
      format. Then it requests a barcode from the image analyzer and slaps the barcode onto a text
      label if it's valid.
     */
-//    @IBAction func analyzePictureTouchedUpInside(_ sender: Any) {
-//        /*
-//         We start by converting the image from a UIImage? to a CIImage.
-//         TODO: we should probably add some code here that throws up a little alert asking for the user
-//         to try again, since it's obviously their fault and my code is perfect.
-//        */
-//        guard let image = currentImage else {return}
-//        guard let convertedImage = CIImage(image: image) else {return}
-//
-//        /*
-//         Then, we'll initialize a VNImageRequestHandler
-//         (I don't know how the options work, and it seems to work fine without them)
-//         that we'll pass the newly converted image. The image will be in the .up orientation
-//         (the user should take the photo vertically, maybe we should add a check for this? idk.)
-//        */
-//        let imageAnalyzer = VNImageRequestHandler(ciImage: convertedImage, orientation: .up, options: [:])
-//        let barcodeRequest = VNDetectBarcodesRequest()
-//
-//        do {
-//            try imageAnalyzer.perform([barcodeRequest]) //we're passing a single barcode request, since that's all we care about.
-//        } catch {
-//            //empty catch statement, we should add a little alert here too if the analysis fails
-//        }
-//
-//        if let results = barcodeRequest.results { //if the code works, then slap the last element of the results up on the label on the screen
-//            if results.count > 0, let barcode = results[0] as? VNBarcodeObservation {
-//                let databaseReq = DatabaseRequests(barcodeString: barcode.payloadStringValue!, beforeLoading: {self.startLoad()}, afterLoading: {self.endLoad()})
-//                self.barcodeText.text = databaseReq.result
-//            }
-//        }
-//    }
+    @IBAction func analyzePictureTouchedUpInside(_ sender: Any) {
+        /*
+         We start by converting the image from a UIImage? to a CIImage.
+         TODO: we should probably add some code here that throws up a little alert asking for the user
+         to try again, since it's obviously their fault and my code is perfect.
+        */
+        guard let image = currentImage else {return}
+        guard let convertedImage = CIImage(image: image) else {return}
+        
+        /*
+         Then, we'll initialize a VNImageRequestHandler
+         (I don't know how the options work, and it seems to work fine without them)
+         that we'll pass the newly converted image. The image will be in the .up orientation
+         (the user should take the photo vertically, maybe we should add a check for this? idk.)
+        */
+        let imageAnalyzer = VNImageRequestHandler(ciImage: convertedImage, orientation: .up, options: [:])
+        let barcodeRequest = VNDetectBarcodesRequest()
+        
+        do {
+            try imageAnalyzer.perform([barcodeRequest]) //we're passing a single barcode request, since that's all we care about.
+        } catch {
+            //empty catch statement, we should add a little alert here too if the analysis fails
+        }
+        
+        if let results = barcodeRequest.results { //if the code works, then slap the last element of the results up on the label on the screen
+            if results.count > 0, let barcode = results[0] as? VNBarcodeObservation {
+                let databaseReq = DatabaseRequests(barcodeString: barcode.payloadStringValue!, beforeLoading: {self.startLoad()}, afterLoading: {self.endLoad()})
+                self.barcodeText.text = databaseReq.result
+            }
+        }
+    }
     
     /*
      This action should allow the user to open up a camera view so that they can take a picture for image
      analysis. This may actually have to segue to another screen where there's a camera view, but I'm not so
      sure. For now, it totally works.
      */
+    @IBAction func takePictureTouchedUpInside(_ sender: Any) {
+        if(UIImagePickerController .isSourceTypeAvailable(UIImagePickerControllerSourceType.camera))
+        {
+            analyzePictureButton.isEnabled = true
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera
+            imagePicker.allowsEditing = true
+            self.present(imagePicker, animated: true, completion: nil)
+        } else {
+            barcodeText.text = "no camera recognized" //just to check.
+        }
+    }
     
     /*
      All this function does is close the imagePicker view when the cancel button is hit.
     */
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        imagePicker.dismiss(animated:true, completion: nil) //then cancel the view.
+    }
     
     /*
      This function takes the picked image from the UIImagePickerController (thanks for the code, Swope)
      and sets currentImage equal to this selected image.
     */
-
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerEditedImage] as? UIImage {
+            self.currentImage = pickedImage
+            imagePicker.dismiss(animated: true, completion: nil)
+        } else {
+            //we should probably do something here.
+        }
+    }
     
     func startLoad() {
         self.view.addSubview(loadingPopover)
         loadingPopover.center = self.view.center
+        self.analyzePictureButton.isEnabled = false
+        self.choosePictureButton.isEnabled = false
+        self.takePictureButton.isEnabled = false
         
         let shape1 = smallOpening()
         let shape2 = largeOpening()
@@ -124,17 +160,21 @@ class CameraTestViewController:
     func endLoad() {
         self.loadingPopover.removeFromSuperview()
         self.contractionAnimation?.isRemovedOnCompletion = true
-
+        self.analyzePictureButton.isEnabled = true
+        self.choosePictureButton.isEnabled = true
+        self.takePictureButton.isEnabled = true
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        analyzePictureButton.isEnabled = false //we're going to disable the button until the person uploads an image
+        imagePicker.delegate = self //this line of code is needed, for some weird reason. ¯\_(ツ)_/¯
         self.databaseReq = DatabaseRequests(barcodeString: "075856055399", beforeLoading: {self.startLoad()}, afterLoading: {self.endLoad(); print("done"); self.analysisFinished()})
         
     }
     
     func analysisFinished() {
-        
+        self.barcodeText.text = String((databaseReq!.result?[(databaseReq?.result?.startIndex)!...(databaseReq?.result?.index((databaseReq?.result?.startIndex)!, offsetBy: 10))!])!)
     }
     
 
