6cb7c0b91023f4b213becc468ad70d7040cd0668
Annie Liang (student LM) <s015205@lm-20s015205.lmsd.org>
2019-02-08 12:56:38 -0500

    Added Calories View Controller

---
+++ CareIt/CaloriesViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CaloriesViewController.swift
+//  CareIt
+//
+//  Created by Annie Liang (student LM) on 2/8/19.
+//  Copyright © 2019 Jason Kozarsky (student LM). All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CaloriesViewController: UIViewController{
+    
+}
