ff506dc265c3ac4e5d90c88d4123d0a3b7d9ff0d
marshmallowsole <kwcarolina1@gmail.com>
2019-02-22 13:38:29 -0500

    Calendar month week complete, need to delete some formatting things

--- CareIt/MonthView.swift
+++ CareIt/MonthView.swift
@@ -20,14 +20,14 @@ class MonthView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.backgroundColor = UIColor.clear
+        self.backgroundColor=UIColor.clear
         
         currentMonthIndex = Calendar.current.component(.month, from: Date()) - 1
         currentYear = Calendar.current.component(.year, from: Date())
         
         setupViews()
         
-        btnLeft.isEnabled = false
+        btnLeft.isEnabled=false
     }
     
     @objc func btnLeftRightAction(sender: UIButton) {
@@ -37,29 +37,69 @@ class MonthView: UIView {
                 currentMonthIndex = 0
                 currentYear += 1
             }
-        }
-        else {
+        } else {
             currentMonthIndex -= 1
             if currentMonthIndex < 0 {
                 currentMonthIndex = 11
                 currentYear -= 1
             }
         }
-        lblName.text = "\(monthsArr[currentMonthIndex]) \(currentYear)"
+        lblName.text="\(monthsArr[currentMonthIndex]) \(currentYear)"
         delegate?.didChangeMonth(monthIndex: currentMonthIndex, year: currentYear)
     }
     
-    func setupVews() {
-        //constraints + buttons
+    func setupViews() {
+        self.addSubview(lblName)
+        lblName.topAnchor.constraint(equalTo: topAnchor).isActive=true
+        lblName.centerXAnchor.constraint(equalTo: centerXAnchor).isActive=true
+        lblName.widthAnchor.constraint(equalToConstant: 150).isActive=true
+        lblName.heightAnchor.constraint(equalTo: heightAnchor).isActive=true
+        lblName.text="\(monthsArr[currentMonthIndex]) \(currentYear)"
+        
+        self.addSubview(btnRight)
+        btnRight.topAnchor.constraint(equalTo: topAnchor).isActive=true
+        btnRight.rightAnchor.constraint(equalTo: rightAnchor).isActive=true
+        btnRight.widthAnchor.constraint(equalToConstant: 50).isActive=true
+        btnRight.heightAnchor.constraint(equalTo: heightAnchor).isActive=true
+        
+        self.addSubview(btnLeft)
+        btnLeft.topAnchor.constraint(equalTo: topAnchor).isActive=true
+        btnLeft.leftAnchor.constraint(equalTo: leftAnchor).isActive=true
+        btnLeft.widthAnchor.constraint(equalToConstant: 50).isActive=true
+        btnLeft.heightAnchor.constraint(equalTo: heightAnchor).isActive=true
     }
     
-    let lblName: UILabel
+    let lblName: UILabel = {
+        let lbl=UILabel()
+        lbl.text="Default Month Year text"
+        lbl.textColor = Style.monthViewLblColor
+        lbl.textAlignment = .center
+        lbl.font=UIFont.boldSystemFont(ofSize: 16)
+        lbl.translatesAutoresizingMaskIntoConstraints=false
+        return lbl
+    }()
     
-    let btnRight: UIButton
+    let btnRight: UIButton = {
+        let btn=UIButton()
+        btn.setTitle(">", for: .normal)
+        btn.setTitleColor(Style.monthViewBtnRightColor, for: .normal)
+        btn.translatesAutoresizingMaskIntoConstraints=false
+        btn.addTarget(self, action: #selector(btnLeftRightAction(sender:)), for: .touchUpInside)
+        return btn
+    }()
     
-    let btnLeft = UIButton
+    let btnLeft: UIButton = {
+        let btn=UIButton()
+        btn.setTitle("<", for: .normal)
+        btn.setTitleColor(Style.monthViewBtnLeftColor, for: .normal)
+        btn.translatesAutoresizingMaskIntoConstraints=false
+        btn.addTarget(self, action: #selector(btnLeftRightAction(sender:)), for: .touchUpInside)
+        btn.setTitleColor(UIColor.lightGray, for: .disabled)
+        return btn
+    }()
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
+
