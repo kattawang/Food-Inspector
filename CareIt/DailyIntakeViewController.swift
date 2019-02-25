51861a8c17fc7cb4319914efabea60e6940471d8
marshmallowsole <kwcarolina1@gmail.com>
2019-02-25 14:37:02 -0500

    Cleaning up some code

---
+++ CareIt/DailyIntakeViewController.swift
@@ -0,0 +1,54 @@
+//
+//  DailyIntakeViewController.swift
+//  CareIt
+//
+//  Created by Katherine Wang (student LM) on 2/25/19.
+//  Copyright © 2019 Jason Kozarsky (student LM). All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DailyIntakeViewController: UIView, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout/*, MonthViewDelegate*/ {
+    
+    var numOfDaysInMonth = [31,28,31,30,31,30,31,31,30,31,30,31]
+    var currentMonthIndex: Int = 0
+    var currentYear: Int = 0
+    var presentMonthIndex = 0
+    var presentYear = 0
+    var todaysDate = 0
+    var firstWeekDayOfMonth = 0   //(Sunday-Saturday 1-7)
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
+}
+
+//get first day of the month
+extension Date {
+    var weekday: Int {
+        return Calendar.current.component(.weekday, from: self)
+    }
+    var firstDayOfTheMonth: Date {
+        return Calendar.current.date(from: Calendar.current.dateComponents([.year,.month], from: self))!
+    }
+}
+
+//get date from string
+extension String {
+    static var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter
+    }()
+    
+    var date: Date? {
+        return String.dateFormatter.date(from: self)
+    }
+}
