c9d07988976435530a1f8f9a76cfe4bf8124ad68
Jason Kozarsky <jasonkozarsky19@students.lmsd.org>
2019-03-01 14:33:24 -0500

    fixed segue issues from homescreen view controller

--- CareIt/HomeScreenViewController.swift
+++ CareIt/HomeScreenViewController.swift
@@ -64,7 +64,6 @@ class HomeScreenViewController: UIViewController {
         cameraButton.translatesAutoresizingMaskIntoConstraints = false
         cameraButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         cameraButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
-        
     }
     
     @objc func logoutTouchedUp() {
