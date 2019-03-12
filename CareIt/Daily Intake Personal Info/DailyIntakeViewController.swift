0e8d77da3682865b7b7915222d0d1f34bdba0081
marshmallowsole <kwcarolina1@gmail.com>
2019-03-12 10:27:44 -0400

    Deleted Calories, consolidating classes into Daily Intake

--- CareIt/Daily Intake Personal Info/DailyIntakeViewController.swift
+++ CareIt/Daily Intake Personal Info/DailyIntakeViewController.swift
@@ -67,6 +67,41 @@ class DailyIntakeViewController: UIViewController, UICollectionViewDelegate, UIC
         print(day)
         print(year)
         print(DaysOfMonth[weekday])
+        
+        // added this stuff need to check
+        let ref: DatabaseReference!
+        
+//        ref.observeSingleEvent(of: .value, with: { snapshot in
+//            for child in snapshot.children {
+//                if let sex = snapshot.value["Sex"] as? String {
+//                    print(sex)
+//                }
+//            }
+//        }
+        
+        
+        
+        //To do: ask jason about getting calories
+        
+        //        let ageComponents = calendar.dateComponents([.year], from: birthday, to: now)
+        //        let age = ageComponents.year!
+        //
+        //        if (sex=="Female") {
+        //            recomCalories = 10*(weight/2.20462) + 6.25*(height/0.393701) - 5*age - 161
+        //        }
+        //        else {
+        //            recomCalories = 10*(weight/2.20462) + 6.25*(height/0.393701) - 5*age + 5
+        //        }
+        //
+        //        if (activity == "Low") {
+        //            recomCalories *= 1.2
+        //        }
+        //        else if (activity == "Medium") {
+        //            recomCalories *= 1.3
+        //        }
+        //        else {
+        //            recomCalories *= 1.4
+        //        }
     }
     
     
@@ -223,6 +258,8 @@ class DailyIntakeViewController: UIViewController, UICollectionViewDelegate, UIC
         
         
         //do display nutrient info stuff here
+//        recomCalories.text = userInfo[""]
+        
         
     }
     
