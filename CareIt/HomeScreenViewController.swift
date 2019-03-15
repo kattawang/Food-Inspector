2b30e581586ae094b825c091ad1c62cf37a2e2bf
Annie Liang (student LM) <s015205@lm-20s015205.local>
2019-03-15 12:46:36 -0400

    Edited constraints for background icons for home screen buttons

--- CareIt/HomeScreenViewController.swift
+++ CareIt/HomeScreenViewController.swift
@@ -53,7 +53,6 @@ class HomeScreenViewController: UIViewController {
         cameraButton.addTarget(self, action: #selector(cameraTouchedUp), for: .touchUpInside)
         cameraButton.setBackgroundImage(UIImage(named: "Barcode"), for: .normal)
         
-        
         //constraints for buttons
         logoutButton.translatesAutoresizingMaskIntoConstraints = false
         logoutButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
