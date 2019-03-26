1adc2ed0a1d99294edb8c35bb578a33c82f4f805
Jason Kozarsky <s012795@lm-19s012795.lmsd.org>
2019-03-26 14:38:34 -0400

    Got the story board to work

--- CareIt/Log In Sign Up/SignUpViewController.swift
+++ CareIt/Log In Sign Up/SignUpViewController.swift
@@ -37,7 +37,7 @@ class SignUpViewController: UIViewController, UITextFieldDelegate {
     //Sets delegates
     override func viewDidLoad() {
         super.viewDidLoad()
-        snavigationController?.navigationBar.barTintColor = view.backgroundColor
+        navigationController?.navigationBar.barTintColor = view.backgroundColor
         emailTextField.delegate = self  
         passwordTextField.delegate = self
         emailTextField.becomeFirstResponder()
